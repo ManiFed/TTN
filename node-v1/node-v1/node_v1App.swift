0873729eff4daaf23e3b41f4e4fa1e607a7ed836
Evan Goldfine <evangoldfine@Mac.fios-router.home>
2026-06-27 15:40:22 -0400

    xcode

---
+++ node-v1/node-v1/node_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  node_v1App.swift
+//  node-v1
+//
+//  Created by Evan Goldfine on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct node_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
